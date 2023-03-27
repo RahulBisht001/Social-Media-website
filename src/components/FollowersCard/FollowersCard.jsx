1146499173693cd14c28aa20778bf9ee7106d189
RahulB <rahulbisht1012@gmail.com>
2023-03-27 19:00:38 +0530

    FollowerCard Completed

--- src/components/FollowersCard/FollowersCard.jsx
+++ src/components/FollowersCard/FollowersCard.jsx
@@ -1,12 +1,31 @@
 import React from 'react'
 
 import './FollowersCard.css'
-
+import { Followers } from '../../data/FollowersData'
 
 const FollowersCard = () => {
     return (
         <div className="FollowersCard">
             <h4>who is following you</h4>
+
+            {
+                Followers.map((Follower, id) => {
+                    return (
+                        <div className="follower">
+                            <div>
+                                <img src={Follower.img} alt="" className='followerImage' />
+                                <div className="name">
+                                    <span>{Follower.name}</span>
+                                    <span>@{Follower.username}</span>
+                                </div>
+                            </div>
+                            <button className='button fc-button'>
+                                Follow
+                            </button>
+                        </div>
+                    )
+                })
+            }
         </div>
     )
 }
---
+++ src/data/FollowersData.js
@@ -0,0 +1,21 @@
+import img1 from "../img/img1.png";
+import img2 from "../img/img2.png";
+import img3 from "../img/img3.png";
+import img4 from "../img/img4.jpg";
+
+export const Followers = [
+
+    { name: "Andrew Thomas", username: "AndrewThomas", img: img1 },
+    { name: "Hulk Buster", username: "HulkBuster", img: img2 },
+    { name: "Thor", username: "ThunderMaster", img: img3 },
+    { name: "Natasha", username: "Natasha", img: img4 },
+    { name: "Emma Johnson", username: "EmmaJohnson", img: img2 },
+    { name: "Oliver Lee", username: "OliverLee", img: img3 },
+    { name: "Sophie Wilson", username: "SophieWilson", img: img1 },
+    { name: "William Davis", username: "WilliamDavis", img: img2 },
+    { name: "Ella Smith", username: "EllaSmith", img: img4 },
+    { name: "Jack Brown", username: "JackBrown", img: img1 },
+    { name: "Grace Taylor", username: "GraceTaylor", img: img2 },
+    { name: "Noah White", username: "NoahWhite", img: img1 },
+    { name: "Mia Green", username: "MiaGreen", img: img3 },
+];
