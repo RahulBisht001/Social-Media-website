fa1824eb03e5e2a449553f9cc901968de9b8e86f
RahulB <rahulbisht1012@gmail.com>
2023-03-27 08:26:44 +0530

    Social Media App

---
+++ src/pages/Home/Home.jsx
@@ -0,0 +1,15 @@
+import React from 'react'
+import './Home.css'
+
+
+const Home = () => {
+    return (
+        <div className="Home">
+            <div className="profileSide">profile</div>
+            <div className="postSide">Post</div>
+            <div className="rightSide">right</div>
+        </div>
+    )
+}
+
+export default Home
